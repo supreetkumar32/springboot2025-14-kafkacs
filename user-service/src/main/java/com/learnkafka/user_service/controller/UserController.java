1451bfddc9a3d12772827cdbc9c8958174ab5367
Supreet <supreetkumar32@gmail.com>
2026-05-23 21:15:06 +0530

    producer part is done

--- user-service/src/main/java/com/learnkafka/user_service/controller/UserController.java
+++ user-service/src/main/java/com/learnkafka/user_service/controller/UserController.java
@@ -2,6 +2,7 @@ package com.learnkafka.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+    @Value("${kafka.topic.user-random-topic}")
+    private String KAFKA_RANDOM_USER_TOPIC;
+
     private final KafkaTemplate<String,String> kafkaTemplate;
 
     @PostMapping("/{message}")
     public ResponseEntity<String> sendMessage(@PathVariable String message){
-        kafkaTemplate.send("user-random-topic",message);
+        kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC,message);
         return ResponseEntity.ok("message queued");
     }
 }
